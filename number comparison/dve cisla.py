98e08c18ce2578157446d5cf4e4f2fa0df6ea84c
Gusticek <gugoschneider8@gmail.com>
2023-01-24 09:11:51 +0100

    Update dve cisla.py

--- number comparison/dve cisla.py
+++ number comparison/dve cisla.py
@@ -1,9 +1,13 @@
-a = input("Enter a number: ")
-b = input("Enter a second number: ")
+try:
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a second number: "))
+    
 
-if a>b:
-    print(a)
-elif b>a:
-    print(b)
-else:
-    print("They are the same.")
+    if a>b:
+        print(a)
+    elif b>a:
+        print(b)
+    else:
+        print("They are the same.")
+except ValueError:
+    print("Looks like one or both the numbers are not integers.")
